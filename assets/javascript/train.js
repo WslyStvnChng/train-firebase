c80313e3bd061568c2d0cf088b035969cd04b41f
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-01-11 19:47:02 -0700

    Clock working functional with current time

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,3 +1,21 @@
+function startTime() {
+  var today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+
+  minutes = checkTime(minutes);
+  seonds = checkTime(seconds);
+  document.getElementById("hello").innerHTML = hours + ":" + minutes + ":" + seconds;
+  var time = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCNksDk64_EDLqbzbzKdAnnTqIW4sW_niE",
@@ -22,28 +40,28 @@
     // console.log(snapshot.val().firstTime);
     // console.log(snapshot.val().frequency);
 
-    //
-
     //make variables to make it easy to grab information. Need first train and duration 
     var newwerFirstTime = (snapshot.val().firstTime);
     var newwerFrequency = (snapshot.val().frequency);
     var currentTime = moment();
-    // console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+    console.log("Current Time: " + moment(currentTime).format("hh:mm"));
     var nextArrival;
     var minutesAway;
 
-    // 
     var firstTimeConverted = moment(newwerFirstTime, "hh:mm").subtract(1, "years");
       console.log(firstTimeConverted);
 
-    var tRemainder = firstTimeConverted % currentTime;
+    var anotherTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("Difference in Time: " + anotherTime);
+
+    var tRemainder = anotherTime % newwerFrequency;
       console.log(tRemainder);
 
     var tMinutesTillTrain = newwerFrequency - tRemainder;
       console.log("Minutes Till Train: " + tMinutesTillTrain);
 
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-      console.log("Arrival Time: " + nextTrain);
+      console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
 
     //temprete literal to make the rows
     var newRow = `<tr>
